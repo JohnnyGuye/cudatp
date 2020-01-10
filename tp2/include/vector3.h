278e6d52ed5f6940dd81d69fd029c910e689f950
Guye Quentin <qguye@if501-208-17.insa-lyon.fr>
2020-01-10 11:32:38 +0100

    GGX :D

--- tp2/include/vector3.h
+++ tp2/include/vector3.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#define LOOP_I for(unsigned i = 0; i < 3; i++)
 class Vector3 {
 public:
         union {
@@ -43,6 +44,128 @@ public:
             return *this;
         }
 
+        GCPU_F float & operator[]( const int i ) {
+            return values[i];
+        }
+
+        GCPU_F float operator[]( const int i ) const {
+            return values[i];
+        }
+
+        GCPU_F Vector3 & operator *= ( const float s ) {
+            LOOP_I values[i] *= s;
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator /= ( const float s ) {
+            LOOP_I values[i] /= s;
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator += ( const float s ) {
+            LOOP_I values[i] += s;
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator -= ( const float s ) {
+            LOOP_I values[i] -= s;
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator += ( const Vector3 & v ) {
+            LOOP_I values[i] += v[i];
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator -= ( const Vector3 & v ) {
+            LOOP_I values[i] -= v[i];
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator *= ( const Vector3 & v ) {
+            LOOP_I values[i] *= v[i];
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator /= ( const Vector3 & v ) {
+            LOOP_I values[i] /= v[i];
+            return *this;
+        }
+
+        GCPU_F Vector3 & operator - () {
+            LOOP_I values[i] = -values[i];
+            return *this;
+        }
+
+        GCPU_F Vector3 operator + ( const float s ) const {
+            auto v = *this;
+            v += s;
+            return v;
+        }
+
+        GCPU_F Vector3 operator - ( const float s ) const {
+            auto v = *this;
+            v -= s;
+            return v;
+        }
+        GCPU_F Vector3 operator * ( const float s ) const {
+            auto v = *this;
+            v *= s;
+            return v;
+        }
+        GCPU_F Vector3 operator / ( const float s ) const {
+            auto v = *this;
+            v /= s;
+            return v;
+        }
+
+        GCPU_F Vector3 operator + ( const Vector3 & oth ) const {
+            auto v = *this;
+            v += oth;
+            return v;
+        }
+
+        GCPU_F Vector3 operator - ( const Vector3 & oth ) const {
+            auto v = *this;
+            v -= oth;
+            return v;
+        }
+
+        GCPU_F Vector3 operator * ( const Vector3 & oth ) const {
+            auto v = *this;
+            v *= oth;
+            return v;
+        }
+
+        GCPU_F Vector3 operator / ( const Vector3 & oth ) const {
+            auto v = *this;
+            v /= oth;
+            return v;
+        }
+
+        GCPU_F float length2() const {
+            float n = 0;
+            LOOP_I n += SQR( values[i] );
+            return n;
+        }
+
+        GCPU_F float length() const {
+            return sqrt( length2() );
+        }
+
+        GCPU_F Vector3 & normalize() {
+            auto l = length();
+            if( l ) {
+                *this /= l;
+            }
+            return *this;
+        }
+
+        GCPU_F Vector3 normalized() const {
+            auto v = *this;
+            return v.normalize();
+        }
+
         GCPU_F static float distance2( const Vector3 & lhs, const Vector3 &rhs ) {
                 return SQR(lhs.r - rhs.r) + SQR(lhs.g - rhs.g) + SQR(lhs.b - rhs.b);
         }
@@ -56,8 +179,13 @@ public:
                 return Vector3( v.r / sqr, v.g / sqr, v.b / sqr );
         }
 
-        GCPU_F float operator[]( int i ) {
-            return values[i];
+        GCPU_F Vector3 & clamp( const float min, const float max ) {
+            LOOP_I {
+                if( values[i] <= min ) values[i] = min;
+                if( values[i] >= max ) values[i] = max;
+            }
+
+            return *this;
         }
 
         friend std::ostream & operator << ( std::ostream & os, const Vector3 & vec ) {
--- tp2/raytracer.cu
+++ tp2/raytracer.cu
@@ -1,6 +1,7 @@
 #include "wb.h"
 
 #define SQR(x) ((x)*(x))
+#define SQR5(x) ((x)*(x)*(x)*(x)*(x))
 
 #include "cuda.def.cuh"
 #include "vector3.h"
@@ -8,24 +9,135 @@
 #include <cmath>
 
 
-
-
-class Sphere {
+class Ray {
 public:
-	float size;
-	Vector3 center;
-        Vector3 color;
+    Vector3 origin;
+    Vector3 direction;
 
-        GCPU_F Sphere()
-            : size(1), color( 1.0, 0.3, 0.4 ), center( 0.0 ){}
+    GCPU_F Ray( const Vector3 & origin, const Vector3 & direction )
+        : origin( origin ), direction( direction ) {}
 
-        GCPU_F Sphere( Sphere & sphere )
-            : size( sphere.size ), center( sphere.center ), color( sphere.color ) {}
+    GCPU_F Ray( const Vector3 & direction )
+        : Ray( 0, direction ) {}
+
+    GCPU_F Ray( )
+        : Ray( Vector3(1, 0, 0) ) {}
 
 };
 
+class Material {
+public:
+    Vector3 albedo;
+    float roughness = 0;
+    float metalness = 0;
+};
 
-__global__ void raytrace( Vector3 * image, Sphere * spheres, int width, int height, int sphereCount ) {
+class Sphere {
+public:
+        float size = 1;
+	Vector3 center;
+        Material material;
+
+        GCPU_F Sphere()
+            : size(1), center( 0.0 ){}
+
+        GCPU_F Sphere( Sphere & sphere )
+            : size( sphere.size ), center( sphere.center ), material( sphere.material ) {}
+
+        GCPU_F Vector3 normalAt( const Vector3 & hit ) const {
+            return (hit - center).normalize();
+        }
+
+        GCPU_F bool intersects( const Vector3 & origin, const Vector3 & direction, Vector3 & hit, float & distance ) const {
+            auto m = origin - center;
+            float b = Vector3::dotProduct( m, direction );
+            float c = m.length2() - SQR( size );
+
+            if( c > 0.0f && b > 0.0f ) return false;
+            float discr = SQR( b ) - c;
+
+            if( discr < 0.0f ) return false;
+            auto t = -b - sqrt( discr );
+
+            if( t < 0.0f ) t = -b + sqrt( discr );
+
+            hit = origin + direction * t;
+            distance = t;
+
+            return true;
+        }
+
+        friend std::ostream & operator << ( std::ostream & os, const Sphere & sphere ) {
+            os << "Sphere: radius: " << sphere.size
+               << " center: " << sphere.center
+               << " color: " << sphere.material.albedo;
+            return os;
+        }
+};
+
+class Light {
+public:
+    Vector3 origin;
+    Vector3 color = 1;
+    float intensity = 1;
+
+    Light() {}
+};
+
+class Collision {
+public:
+
+    Sphere * sphere = nullptr;
+    Vector3 normal;
+    Vector3 hit;
+    float distance = 2 * 1e38;
+
+};
+
+GCPU_F bool getFirstCollision( const Ray & ray, Sphere * spheres, const int sphereCount, Collision & collision ) {
+
+    collision.distance = 2e38;
+    collision.sphere = nullptr;
+
+    auto collided = false;
+
+    for( unsigned int i = 0; i < sphereCount; i++ ) {
+        Vector3 hit;
+        float distance;
+        if( !spheres[i].intersects( ray.origin, ray.direction, hit, distance ) ) continue;
+        if( collision.distance < distance ) continue;
+
+        collision.distance = distance;
+        collision.hit = hit;
+        collision.normal = spheres[i].normalAt( hit );
+        collision.sphere = &spheres[i];
+        collided = true;
+
+    }
+
+    return collided;
+}
+
+GCPU_F float GGXDistribution( float alpha2, float NdotH ) {
+    return alpha2 / (M_PI * SQR( NdotH ) * SQR((alpha2 - 1) + 1) );
+}
+
+GCPU_F float GGXGeometry( float alpha2, float NdotL ) {
+    return 2 * NdotL / (NdotL + sqrt(alpha2 + (1-alpha2) * SQR(NdotL) ));
+}
+
+GCPU_F Vector3 Fresnel( const Vector3 & f0, const float NdotH ) {
+    auto unary = Vector3(1);
+    return f0 + (unary - f0) * (SQR5(unary - NdotH));
+}
+
+GCPU_F float dodgeZero( float value ) {
+    if( value  < 0 ) value = 0;
+    if( value < 0.001 ) value = 0.001;
+    return value;
+}
+
+__global__ void raytrace( Vector3 * image, Sphere * spheres, Light * lights, int width, int height, int sphereCount, int lightCount ) {
 
         int idx = threadIdx.x
                 + threadIdx.y * blockDim.y
@@ -37,34 +149,55 @@ __global__ void raytrace( Vector3 * image, Sphere * spheres, int width, int heig
         
 
         int i = idx % width;
-        int j = idx / height;
+        int j = height - idx / height;
 
 	//printf("%d %d %d\n", i, j, idx ); 
 	
         //printf("-- %d %d %d\n", spheres[0].center.r, spheres[0].center.g, spheres[0].center.b );
         Vector3 pos = Vector3( i, j, 0 );
+        Ray ray;
+        ray.origin = Vector3( i, j, -100 );
+        ray.direction = Vector3( 0, 0, 1 );
 
         image[ idx ] = Vector3( 1 - (j / (float)height) );
 
-        for( auto i = 0; i < sphereCount; i++ ) {
+        Collision collision;
+        if( !getFirstCollision( ray, spheres, sphereCount, collision ) ) return;
 
-            auto sphere = spheres[i];
-            auto dist2 = Vector3::distance2( pos, sphere.center );
-            auto sqrSize2 = SQR( sphere.size );
+        //image[ idx ] = collision.sphere->color;
 
-            //auto a = (image[ idx ] = Vector3( height,123,156 ) );
-            //printf("%d\n", a.r  );
-            if( dist2 < sqrSize2 ){
+        auto material = collision.sphere->material;
+        auto albedo = material.albedo;
+        auto rough = material.roughness * 0.90 + 0.0;
+        auto metal = material.metalness;
+        auto f0 = albedo * 0.04;
 
-                    image[ idx ] = sphere.color;
-                    image[ idx ].r *= 1 - dist2 / sqrSize2;
-                    image[ idx ].g *= 1 - dist2 / sqrSize2;
-                    image[ idx ].b *= 1 - dist2 / sqrSize2;
+        auto alpha = SQR( rough );
+        auto alpha2 = SQR( alpha );
 
-            }
+        image[ idx ] = 0;
 
+        for( unsigned int i = 0; i < lightCount; i++ ) {
+            auto & light = lights[i];
+            auto color = light.color * light.intensity;
+            auto L = (light.origin - collision.hit).normalize();
+            auto N = collision.normal;
+            auto V = -ray.direction.normalized();
+            auto H = (L + V).normalize();
+
+            auto NdotL = dodgeZero( Vector3::dotProduct( N, L ) );
+            auto NdotV = dodgeZero( Vector3::dotProduct( N, V ) );
+            auto NdotH = dodgeZero( Vector3::dotProduct( N, H ) );
+
+            auto D = GGXDistribution( alpha2, NdotH );
+            auto G = GGXGeometry( alpha2, NdotL ) * GGXGeometry( alpha2, NdotV );
+            auto F = Fresnel( f0, NdotH );
+            auto denom = 4 * NdotL * NdotV;
+
+            color *= albedo * F * G * D/ denom * NdotL;
+
+            image[idx] += color;
         }
-
 	
 }
 
@@ -74,10 +207,13 @@ void writeP3( std::string fileName, Vector3 * data, int width, int height ) {
 	file.open( fileName );
 	file << "P3\n" << width << " " << height << "\n" << 255 << std::endl;
 	
-	for( auto idx = 0; idx < width * height; idx++ ) {
-		file << (int)(data[ idx ].r * 255) << " "
-			<< (int)(data[ idx ].g * 255) << " "
-			<< (int)(data[ idx ].b * 255) << " ";
+        for( auto idx = 0; idx < width * height; idx++ ) {
+            auto d = data[ idx ];
+            d.clamp( 0, 1 );
+            std::cout << d * 255 << std::endl;
+            file << (int)(d.r * 255) << " "
+                    << (int)(d.g * 255) << " "
+                    << (int)(d.b * 255) << " ";
 	}
 
 	file << std::flush;
@@ -86,6 +222,10 @@ void writeP3( std::string fileName, Vector3 * data, int width, int height ) {
 
 class Scene {
 public:
+
+    Light * lights;
+    int lightCount = 0;
+
     Sphere * spheres;
     int sphereCount = 0;
 
@@ -93,19 +233,38 @@ public:
 
     GCPU_F ~Scene() {
         if( sphereCount != 0 ) delete [] spheres;
+        if( lightCount != 0 ) delete [] lights;
     }
 
-    GCPU_F Scene & addSphere( const Sphere & sphere ) {
+    Scene & addSphere( const Sphere & sphere ) {
         Sphere * spheres = new Sphere[ sphereCount + 1 ];
         for( unsigned int i = 0; i < sphereCount; i++ ) {
             spheres[i] = this->spheres[i];
         }
+
         spheres[sphereCount] = sphere;
         if( sphereCount ) {
-            delete [] spheres;
+            delete [] this->spheres;
         }
+
         this->spheres = spheres;
         sphereCount++;
+        return *this;
+    }
+
+    Scene & addLight( const Light & light ) {
+        Light * lights = new Light[ lightCount + 1 ];
+        for( unsigned int i = 0; i < lightCount; i++ ) {
+            lights[i] = this->lights[i];
+        }
+
+        lights[lightCount] = light;
+        if( lightCount ) {
+            delete [] this->lights;
+        }
+
+        this->lights = lights;
+        lightCount++;
         return *this;
     }
 
@@ -113,16 +272,40 @@ public:
 
 void populateScene( Scene & scene ) {
 
-    Sphere sphere1;
-    sphere1.center = Vector3( 140, 130, 0 );
-    sphere1.size = 100;
+    auto steps = 5;
+    auto stepW = 300 / (steps + 2);
+    auto stepH = 300 / (steps + 2);
+    for( unsigned int i = 0; i <= steps; i++ ) {
+        for( unsigned int j = 0; j <= steps; j++ ) {
 
-    Sphere sphere2;
-    sphere2.center = Vector3( 210, 230, 0 );
-    sphere2.size = 20;
+            Sphere sphere;
+            sphere.center   = Vector3( stepW * (i + 1), stepH * (j + 1), 0 );
+            sphere.size     = min( stepW, stepH ) / 2.5f;
+            sphere.material.albedo = Vector3(1, 0, 0.5);
+            sphere.material.roughness = (float)i/(float)steps;
+            sphere.material.metalness = (float)j/(float)steps;
 
-    scene.addSphere( sphere2 );
-    scene.addSphere( sphere1 );
+            scene.addSphere( sphere );
+        }
+    }
+//    Sphere sphere1;
+//    sphere1.center = Vector3( 140, 130, 0 );
+//    sphere1.size = 100;
+//    sphere1.material.albedo = Vector3( 1, 0, 0 );
+
+//    Sphere sphere2;
+//    sphere2.center = Vector3( 210, 230, 0 );
+//    sphere2.size = 20;
+//    sphere2.material.albedo = Vector3( 0, 0, 1 );
+
+//    scene.addSphere( sphere1 );
+
+//    scene.addSphere( sphere2 );
+
+    Light light1;
+    light1.origin = Vector3( 80, 200, -100 );
+
+    scene.addLight( light1 );
 
 }
 
@@ -134,6 +317,7 @@ int main(int argc, char *argv[]) {
 
 	Vector3 * d_imgData;
         Sphere * d_spheres;
+        Light * d_lights;
 
         int width = 300;
         int height = 300;
@@ -146,14 +330,16 @@ int main(int argc, char *argv[]) {
 	
         cudaMalloc( (void **) &d_imgData, pixelCount * sizeof( Vector3 ) );
         cudaMalloc( (void **) &d_spheres, scene.sphereCount * sizeof( Sphere ) );
+        cudaMalloc( (void **) &d_lights, scene.lightCount * sizeof( Light ) );
 
         cudaMemcpy( d_spheres, scene.spheres, scene.sphereCount * sizeof( Sphere ), cudaMemcpyHostToDevice );
+        cudaMemcpy( d_lights, scene.lights, scene.lightCount * sizeof( Light ), cudaMemcpyHostToDevice );
 
         dim3 threaddim = dim3( 32, 32, 1 );
         auto totalBlockCount = ceil( pixelCount / (float)(threaddim.x * threaddim.y) );
         dim3 blockdim = dim3( totalBlockCount, 1, 1 );
 
-        raytrace<<< blockdim, threaddim >>>( d_imgData, d_spheres, width, height, scene.sphereCount );
+        raytrace<<< blockdim, threaddim >>>( d_imgData, d_spheres, d_lights, width, height, scene.sphereCount, scene.lightCount );
 
         CHECK_ERROR
 
@@ -165,6 +351,7 @@ int main(int argc, char *argv[]) {
 
         cudaFree( d_imgData );
         cudaFree( d_spheres );
+        cudaFree( d_lights );
 
         writeP3( "output.ppm", imgData, width, height );
 
